2f7b5f41ab84980f6cef29757420358b82aa0f9c
Rob J Goedman <goedman@mac.com>
2018-08-23 14:51:29 -0400

    Updated StanMCMCChains.jl and .gitignore (added Manifest.toml)

--- src/StanMCMCChains.jl
+++ src/StanMCMCChains.jl
@@ -1,5 +1,25 @@
 module StanMCMCChains
 
 # package code goes here
+using MCMCChains, Statistics, Documenter
 
+import CmdStan: convert_a3d
+#import Mamba: AbstractChains, Chains
+
+#=
+abstract type AbstractChains end
+
+struct Chains <: AbstractChains
+  value::Array{Float64, 3}
+  range::UnitRange{Int}
+  names::Vector{AbstractString}
+  chains::Vector{Int}
+end
+=#
+
+include("utilities/convert_a3d.jl")
+
+export
+  convert_a3d
+  
 end # module
